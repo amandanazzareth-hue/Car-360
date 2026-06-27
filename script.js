1f944a149ceeaa512ea3869d63fea2271f535a13
amandanazzareth-hue <amandanazzareth@gmail.com>
2026-06-27 16:55:30 -0300

    Improve image dragging and update logic
    Refactor image dragging functionality and update image source based on drag direction.

--- script.js
+++ script.js
@@ -1,17 +1,52 @@
 const container = document.querySelector(".container");
-const image = document. querySlector(".car-image");
+const image = document.querySelector(".car-image");
 
 const cursor = {
-    isDragging: true,
+    isDragging: false,
     initialPosition: 0,
+};
+
+let currentImage = 1;
+
+const updateImage = (direction) => {
+    if (direction < 0) {
+        if (currentImage == 12) {
+            currentImage = 1
+        } else {
+            currentImage += 1
+        }
+        
+    }
+
+    if (direction > 0) {
+        if (currentImage == 1) {
+            currentImage = 12
+        } else {
+            currentImage -= 1
+        }
+
+    }
+
+    image.src = `./images/${currentImage}.jpg`;
+
 };
 
  container.addEventListener("mousedown", (event) => {
     cursor.isDragging = true;
-    cursor.initialPosition = event.clientX
+    cursor.initialPosition = event.clientX;
  });
 
-  container.addEventListener("mouseup", (event) => {
-    cursor.isDragging = true;
-    cursor.initialPosition = event.clientX
+ container.addEventListener("mouseup", () => {
+    cursor.isDragging = false;
+ });
+
+ container.addEventListener("mousemove", ({ clientX  }) => {
+    if (!cursor.isDragging) return;
+
+    const offset = cursor.initialPosition - clientX;
+
+    if(Math.abs(offset) >= 50) {
+        updateImage(offset);
+        cursor.initialPosition = clientX;
+    }
  });
